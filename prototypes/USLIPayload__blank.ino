f7b01234097fbfaf418ef026adfc252e759b995d
Peter Fidelman <fidelp@uw.edu>
2011-12-29 15:44:07 -0800

    better readme, backed up skeleton

---
+++ prototypes/USLIPayload__blank.ino
@@ -0,0 +1,19 @@
+/* USLIPayload.ino - Main driver program 
+ * Purpose: Constantly collect data and send via telemetry.
+ * Created by the University of Washington USLI Team, 2010-2011
+ * Released into the public domain - use at your own risk!
+ */
+
+#include <Accelerometer.h>
+#include <Camera.h>
+#include <GPS.h>
+#include <LightIntensity.h>
+#include <Pressure.h>
+#include <TempHumidity.h>
+#include <Xbee.h>
+
+void setup() {
+}
+
+void loop() {
+}
